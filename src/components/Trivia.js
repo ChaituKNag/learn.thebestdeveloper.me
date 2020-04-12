d44b10e3133340d2b34da1091023fc5686860723
nkonada <knc@outlook.in>
2020-04-13 01:53:22 +0530

    Random trivia logic has been completed

--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -1,23 +1,7 @@
 import React from "react"
 
-const Trivia = () => {
-  return (
-    <div>
-      <h2>JavaScript trivia: closure</h2>
-      <p>
-        Closure is one of important concept in JavaScript. It is widely
-        discussed and still confused concept. Let's understand what the closure
-        is.
-      </p>
-      <p>
-        First of all, let's see the definition of the Closure given by Douglas
-        Crockford:{" "}
-        <a href="https://crockford.com/javascript/private.html">
-          crockford.com/javascript/private.html
-        </a>
-      </p>
-    </div>
-  )
+const Trivia = ({ children }) => {
+  return <div>{children}</div>
 }
 
 export default Trivia
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,59 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Trivia from "../components/Trivia"
-import Button from "../components/styled/Button"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Trivia>It all starts here!</Trivia>
-    <Button>Random Trivia</Button>
-  </Layout>
-)
+import Button from "../components/styled/Button"
+import { useStaticQuery, graphql } from "gatsby"
+
+const randomIndex = maxIndex => {
+  return Math.floor(Math.random() * Math.floor(maxIndex))
+}
+
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query AllTrivia {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              topic
+              title
+            }
+            html
+            id
+          }
+        }
+      }
+    }
+  `)
+
+  const [current, setCurrent] = useState()
+
+  useEffect(() => {
+    if (data.allMarkdownRemark.edges) {
+      const len = data.allMarkdownRemark.edges.length
+      const idx = randomIndex(len)
+      setCurrent(idx)
+    }
+  }, [data.allMarkdownRemark.edges])
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {data.allMarkdownRemark.edges.map((trivia, idx) => {
+        return idx === current ? (
+          <div
+            key={trivia.node.id}
+            dangerouslySetInnerHTML={{ __html: trivia.node.html }}
+          />
+        ) : null
+      })}
+      {data.allMarkdownRemark.edges.length > 1 ? (
+        <Button>Random Trivia</Button>
+      ) : null}
+    </Layout>
+  )
+}
 
 export default IndexPage
